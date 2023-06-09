5fc86f218438b241e74015c69c0575f6b51fa900
dev-dongwon <dev.dongwonkim@gmail.com>
2023-06-09 15:32:28 +0900

    Feat: Add postgres env for typeorm

---
+++ common/database/typeorm.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from 'src/api/user/user.entity';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  @Inject(ConfigService)
+  private readonly config: ConfigService;
+
+  public createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.config.get<string>('POSTGRES_HOST'),
+      port: this.config.get<number>('POSTGRES_PORT'),
+      database: this.config.get<string>('POSTGRES_DB'),
+      username: this.config.get<string>('POSTGRES_USER'),
+      password: this.config.get<string>('POSTGRES_PASSWORD'),
+      // entities: [__dirname + '../../**/*.entity{.ts,.js}'],
+      entities: [User],
+      synchronize: true,
+    };
+  }
+}
