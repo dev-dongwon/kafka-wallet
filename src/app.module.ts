4d2664a596fcdf22009ae4c88d85a6a36fbd590b
dev-dongwon <dev.dongwonkim@gmail.com>
2023-06-09 14:51:59 +0900

    Feat: Add docker file to connect application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'WALLET_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'wallet',
+            brokers: ['host.docker.internal:29092'],
+          },
+          consumer: {
+            groupId: 'WALLET_CONSUMER',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
