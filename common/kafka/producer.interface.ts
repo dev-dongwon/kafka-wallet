5f552fc125bc0754eb7858f291661d2e1f198cfe
dev-dongwon <dev.dongwonkim@gmail.com>
2023-06-10 19:03:12 +0900

    remove: remove the kafkajs service lib, not used

--- common/kafka/producer.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Message, TopicMessages } from 'kafkajs';
-
-export interface IProducer {
-  connect: () => Promise<void>;
-  disconnect: () => Promise<void>;
-  send: (message: Message[]) => Promise<void>;
-  // batchSend: (topicMessages: TopicMessages[]) => Promise<void>;
-}
