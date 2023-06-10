2ffce33a2a1e6eb54fcdeae60a60fe828691ee95
dev-dongwon <dev.dongwonkim@gmail.com>
2023-06-10 10:16:45 +0900

    Feat: Add database common module

---
+++ common/entity/base.entity.ts
@@ -0,0 +1,22 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  id: any;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt!: Date | null;
+}
