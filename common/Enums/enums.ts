0eca56ac0f28ee210ba03aaa84b2d43250a1cacb
dev-dongwon <dev.dongwonkim@gmail.com>
2023-06-11 15:14:41 +0900

    Feat: Add transaction type and event type enum

--- common/Enums/enums.ts
+++ common/Enums/enums.ts
@@ -1,4 +1,16 @@
-export enum WalletStatus {
-  ACTIVE = 'active',
+export enum TransactionStatus {
+  COMPLETED = 'completed',
   PENDING = 'pending',
+}
+
+export enum TransactionType {
+  WITHDRAW = 'withdraw',
+  DEPOSIT = 'deposit',
+}
+
+export enum EventType {
+  CREATE_WALLET = 'createWallet',
+  DEPOSIT_OR_WITHDRAW = 'depositOrWithdraw',
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
 }
