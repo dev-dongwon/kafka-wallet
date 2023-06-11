b463988f9d57cafe0e8d633f876187e2dccd848b
dev-dongwon <dev.dongwonkim@gmail.com>
2023-06-11 15:15:21 +0900

    Feat: Add transaction service

--- apps/consumer/src/wallet/wallet.service.ts
+++ apps/consumer/src/wallet/wallet.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { WalletsEntity } from '../../../../common/wallet/entity/wallets.entity';
 import { WalletRepository } from './wallet.repository';
+import { TransactionHistoryEntity } from 'common/wallet/entity/transaction.entity';
+import { DepositOrWithdrawDto, TransactionType } from 'common';
 
 @Injectable()
 export class WalletService {
   constructor(private readonly walletRepository: WalletRepository) {}
 
-  async createUser(balance: number): Promise<WalletsEntity> {
-    return await this.walletRepository.createUser(balance);
+  async createWallet(balance: number): Promise<WalletsEntity> {
+    return await this.walletRepository.createWallet(balance);
+  }
+
+  async depositOrWithdraw(
+    depositOrWithdrawDto: DepositOrWithdrawDto,
+  ): Promise<TransactionHistoryEntity> {
+    const existWallet = await this.walletRepository.findOneWallet(
+      depositOrWithdrawDto.walletId,
+    );
+
+    return await this.walletRepository.transactionDepositOrWithdraw(
+      existWallet,
+      depositOrWithdrawDto,
+    );
   }
 }
